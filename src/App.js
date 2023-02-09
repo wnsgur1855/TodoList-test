bf8bf67a72ee10796ffb4d8703d9050581f9b662
junhyuck park <wnsgur1855@naver.com>
2023-02-09 11:51:20 +0900

    todolist test의 css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [content, setContent] = useState('');
+  const [add, setAdd] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const clickAddHandler = () => {
+    setAdd([...add, { id: add.length + 1, content: content }]);
+  };
+
+  const clickDeletHandler = (id) => {
+    setAdd(add.filter((item) => item.id !== id));
+    alert('dd');
+  };
+
+  const clickCount = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="entireBox">
+      <div className="titleBox">
+        <input
+          value={content}
+          onChange={(event) => {
+            setContent(event.target.value);
+          }}
+          type="text"
+          className="inputBox"
+        />
+        <button onClick={clickAddHandler}>추가하기</button>
+        <br />
+      </div>
+      <div className="todolistBox">Todo List</div> <br />
+      <div className="largeBox">
+        {add.map((item) => {
+          return (
+            <div key={item.id} className="smallBox">
+              {item.content} <br />
+              <button className="countsytle" onClick={clickCount}>
+                👍
+              </button>
+              <button className="buttonStyle" onClick={() => clickDeletHandler(item.id)}>
+                💟
+              </button>
+              <div>👍{count}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
